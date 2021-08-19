88aa2fa5cc2975b507e81d82ee3100f17f2d2798
Benjamin Dordoigne <benjamindordoignedesign@gmail.com>
2021-08-19 13:12:19 +0100

    updated project with header items, api calls

--- hulu/Components/HeaderItem/HeaderItem.js
+++ hulu/Components/HeaderItem/HeaderItem.js
@@ -1,6 +1,6 @@
 const HeaderItem = ({ title, Icon }) => {
     return (
-        <div className="flex flex-col items-center cursor-pointer w-12 sm:w-20 hover:text-white group ">
+        <div className="flex flex-col items-center cursor-pointer w-12 sm:w-20 mt-8 hover:text-white group ">
             <Icon className="h-8 mb-1 group-hover:animate-bounce " />
             <p className="opacity-0 group-hover:opacity-100 tracking-widest">{ title }</p>
         </div>
--- hulu/Components/Nav/Nav.js
+++ hulu/Components/Nav/Nav.js
@@ -1,9 +1,19 @@
+import requests from "../../utils/requests"
+
 const Nav = () => {
+
+    console.log(requests);
+
     return (
-        <div>
-            
+
+        <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
+            {Object.entries(requests).map(([key, {title, url }]) => (
+                <h2 className="cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500 last:pr-24" key={key}>{title}</h2>
+                ))}
         </div>
-    )
-}
+    ) 
+
+    
+} 
 
 export default Nav
