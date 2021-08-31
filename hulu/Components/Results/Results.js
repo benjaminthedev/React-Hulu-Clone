389e4194bb329c0250a388cdbd6d5036b634e1d2
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2021-08-31 08:02:49 +0100

    updated build ready for production

--- hulu/Components/Results/Results.js
+++ hulu/Components/Results/Results.js
@@ -1,12 +1,13 @@
 import Thumbnail from "../Thumbnail/Thumbnail"
+import FlipMove from "react-flip-move"
 
 const Results = ({ results }) => {
     return (
-        <div className="px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-4">
+        <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
             {results.map(result => (
                 <Thumbnail key={result.id} result={result} />
             ))}
-        </div>
+        </FlipMove>
     )
 }
 
--- hulu/Components/Thumbnail/Thumbnail.js
+++ hulu/Components/Thumbnail/Thumbnail.js
@@ -1,12 +1,13 @@
 import Image from 'next/image';
 import Overview from '../Overview/Overview';
+import { forwardRef } from 'react';
 
-const Thumbnail = ({ result }) => {
+const Thumbnail = forwardRef(({ result }, ref) => {
 
     const BASE_URL = 'https://image.tmdb.org/t/p/original/';
 
     return (
-        <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-x-105 hover:z-50">
+        <div ref={ref} className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-x-105 hover:z-50">
         {/* Image of item */}
         <Image
             
@@ -23,6 +24,6 @@ const Thumbnail = ({ result }) => {
         <Overview result={result} />
         </div>
     )
-}
+})
 
 export default Thumbnail
